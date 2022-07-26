f402946c835d3b83e226ec8f97290f5caa5d2903
abhishah <abhishah0961@gmail.com>
2022-07-26 15:32:12 +0530

    [routeguard] add authentication to route and [added] dashboard ,auth service

--- frontend/src/app/shared/global-constant.ts
+++ frontend/src/app/shared/global-constant.ts
@@ -4,4 +4,5 @@ export class GlobalConstants {
     public static emailregex: string = "[A-Za-z0-9._%-]+@[A-Za-z0-9._%-]+\\.[a-z]{2,3}"
     public static contactnumberregex: string = "^[e0-9]{10,10}$"
     public static error:string = "error"
+    public static unauthorized:string = "You are not authorized to access this page"
 }
