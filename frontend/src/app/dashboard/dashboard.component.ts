fe03b8c4cd19b206d876f06d43dc17117bf35fdb
abhishah <abhishah0961@gmail.com>
2022-07-26 16:18:33 +0530

    [added] dashboard component

--- frontend/src/app/dashboard/dashboard.component.ts
+++ frontend/src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,8 @@
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 import { Component, AfterViewInit } from '@angular/core';
+import { DashboardService } from '../services/dashboard.service';
+import { SnackbarService } from '../services/snackbar.service';
+import { GlobalConstants } from '../shared/global-constant';
 
 @Component({
 	selector: 'app-dashboard',
@@ -6,9 +10,29 @@ import { Component, AfterViewInit } from '@angular/core';
 	styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements AfterViewInit {
-
+	responsemessage: any
+	data: any
 	ngAfterViewInit() { }
 
-	constructor() {
+	constructor(
+		private dashboardservice: DashboardService,
+		private snackbarservice: SnackbarService
+		
+	) {
+		this.dashboardData()
+	}
+	dashboardData() {
+		this.dashboardservice.getDetails().subscribe((res: any) => {
+			this.data = res
+		}, (error: any) => {
+			console.log(error)
+			if (error.error) {
+				this.responsemessage = error.error
+			}
+			else {
+				this.responsemessage = GlobalConstants.genericerror
+			}
+			this.snackbarservice.opensnackbar(this.responsemessage, GlobalConstants.error)
+		})
 	}
 }
--- frontend/src/app/services/token.interceptor.ts
+++ frontend/src/app/services/token.interceptor.ts
@@ -16,7 +16,7 @@ export class TokenInterceptor implements HttpInterceptor {
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     const token = localStorage.getItem('token')
-    console.log(token)
+    // console.log(token)
     if (token) {
       request = request.clone({
         setHeaders: { Authorization: `Bearer ${token}` }
