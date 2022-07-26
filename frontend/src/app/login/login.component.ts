1935a6cd58ae8b08656e70c74d89c0e41ed38745
abhishah <abhishah0961@gmail.com>
2022-07-26 13:57:09 +0530

    [Added] login service

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatDialogRef } from '@angular/material/dialog';
+import { Router } from '@angular/router';
+
+import { SnackbarService } from '../services/snackbar.service';
+import { UserService } from '../services/user.service';
+import { GlobalConstants } from '../shared/global-constant';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +13,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  loginForm: any = FormGroup
+  responsemessage: any
+  constructor(
+    private formbuilder: FormBuilder,
+    private userservice: UserService,
+    private dialogref: MatDialogRef<LoginComponent>,
+    private snackbarservice: SnackbarService,
+    private router:Router
+  ) { }
 
   ngOnInit(): void {
+    this.loginForm = this.formbuilder.group({
+      email: [null, [Validators.required, Validators.pattern(GlobalConstants.emailregex)]],
+      password: [null, [Validators.required]],
+    })
   }
-
+  onSubmit() {
+    const formData = this.loginForm.value
+    const data = {
+      email: formData.email,
+      password: formData.password
+    }
+    this.userservice.login(data).subscribe((response:any) => {
+      this.responsemessage = response
+      this.dialogref.close()
+      localStorage.setItem('token',response.token)
+      this.router.navigate(['/cafe/dashboard'])
+      // this.snackbarservice.opensnackbar(this.responsemessage, "")
+    }, (error) => {
+      console.log(error)
+      if (error.error) {
+        this.responsemessage = error.error
+      } else {
+  
+        this.responsemessage = GlobalConstants.genericerror
+      }
+      this.snackbarservice.opensnackbar(this.responsemessage,GlobalConstants.error)
+    })
+  }
 }
