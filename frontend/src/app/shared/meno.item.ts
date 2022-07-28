8b8c8f6e09708d7a6ea7cdef05f700e65ee6e407
abhishah <abhishah0961@gmail.com>
2022-07-28 10:10:43 +0530

    [Create] bill component

--- frontend/src/app/shared/meno.item.ts
+++ frontend/src/app/shared/meno.item.ts
@@ -11,7 +11,7 @@ const menuitem = [
     { state: 'dashboard', name: 'Dashboard', icon: 'dashboard', role: '' },
     { state: 'category', name: 'Category', icon: 'category', role: 'admin' },
     { state: 'product', name: 'Product', icon: 'inventory_2', role: 'admin' },
-
+    { state: 'order', name: 'Order', icon: 'list_alt', role: 'admin' },     
 ]
 @Injectable()
 export class MenuItem {
