dd55e95278cd596583c1a90c7d0daf4555430c7d
abhishah <abhishah0961@gmail.com>
2022-07-29 14:28:11 +0530

    [change]backend part

--- backend-cms/services/authentication.js
+++ backend-cms/services/authentication.js
@@ -1,14 +1,14 @@
 require('dotenv').config()
 const jwt = require('jsonwebtoken')
-const authenticationToken = (req,res,next) => {
+const authenticationToken = (req, res, next) => {
     const authHeader = req.headers['authorization']
     const token = authHeader && authHeader.split(' ')[1]
-    if(token ==null){
+    if (token == null) {
         return res.status(401).send('Unauthorized')
     }
-    jwt.verify(token,process.env.ACCESS_TOKEN,(err,response) => {
-        if(err){
-            return res.status(403)
+    jwt.verify(token, process.env.ACCESS_TOKEN, (err, response) => {
+        if (err) {
+            return res.status(403).send('you are unauthorized!, please verify token')
         }
         res.locals = response
         next()
